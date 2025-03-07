b265972fa7378220a1c178c3b341542b8c64f98d
henryflanagan <henrflan986@ed.amdsb.ca>
2025-03-07 14:28:10 -0500

    Add project files.

---
+++ pizza parler/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace pizza_parler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sportGolfBalls = new System.Windows.Forms.Label();
+            this.caluclatingNumbers = new System.Windows.Forms.Button();
+            this.amountBeforeTax = new System.Windows.Forms.Label();
+            this.allTax = new System.Windows.Forms.Label();
+            this.afterTax = new System.Windows.Forms.Label();
+            this.subTotalOutput = new System.Windows.Forms.Label();
+            this.allTaxOutput = new System.Windows.Forms.Label();
+            this.afterTaxOutput = new System.Windows.Forms.Label();
+            this.startNewOrder = new System.Windows.Forms.Button();
+            this.calculatingTheChange = new System.Windows.Forms.Button();
+            this.sportBaseballs = new System.Windows.Forms.Label();
+            this.sportFootball = new System.Windows.Forms.Label();
+            this.numberOfBaseballsInput = new System.Windows.Forms.TextBox();
+            this.enterMoneyInput = new System.Windows.Forms.TextBox();
+            this.numberOfGolfBallsInput = new System.Windows.Forms.TextBox();
+            this.numberOfFootballsInput = new System.Windows.Forms.TextBox();
+            this.moneyGiven = new System.Windows.Forms.Label();
+            this.totalChange = new System.Windows.Forms.Label();
+            this.changeAmountLabel = new System.Windows.Forms.Label();
+            this.receiptLabel = new System.Windows.Forms.Label();
+            this.receiptButton = new System.Windows.Forms.Button();
+            this.storeTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sportGolfBalls
+            // 
+            this.sportGolfBalls.AutoSize = true;
+            this.sportGolfBalls.Location = new System.Drawing.Point(41, 87);
+            this.sportGolfBalls.Name = "sportGolfBalls";
+            this.sportGolfBalls.Size = new System.Drawing.Size(64, 16);
+            this.sportGolfBalls.TabIndex = 1;
+            this.sportGolfBalls.Text = "Golf Balls";
+            // 
+            // caluclatingNumbers
+            // 
+            this.caluclatingNumbers.Location = new System.Drawing.Point(41, 124);
+            this.caluclatingNumbers.Name = "caluclatingNumbers";
+            this.caluclatingNumbers.Size = new System.Drawing.Size(75, 23);
+            this.caluclatingNumbers.TabIndex = 3;
+            this.caluclatingNumbers.Text = "Calculate";
+            this.caluclatingNumbers.UseVisualStyleBackColor = true;
+            this.caluclatingNumbers.Click += new System.EventHandler(this.caluclatingNumbers_Click);
+            // 
+            // amountBeforeTax
+            // 
+            this.amountBeforeTax.AutoSize = true;
+            this.amountBeforeTax.Location = new System.Drawing.Point(49, 172);
+            this.amountBeforeTax.Name = "amountBeforeTax";
+            this.amountBeforeTax.Size = new System.Drawing.Size(56, 16);
+            this.amountBeforeTax.TabIndex = 8;
+            this.amountBeforeTax.Text = "Subtotal";
+            // 
+            // allTax
+            // 
+            this.allTax.AutoSize = true;
+            this.allTax.Location = new System.Drawing.Point(49, 216);
+            this.allTax.Name = "allTax";
+            this.allTax.Size = new System.Drawing.Size(78, 16);
+            this.allTax.TabIndex = 9;
+            this.allTax.Text = "Tax Amount";
+            // 
+            // afterTax
+            // 
+            this.afterTax.AutoSize = true;
+            this.afterTax.Location = new System.Drawing.Point(49, 263);
+            this.afterTax.Name = "afterTax";
+            this.afterTax.Size = new System.Drawing.Size(86, 16);
+            this.afterTax.TabIndex = 10;
+            this.afterTax.Text = "Total Amount";
+            // 
+            // subTotalOutput
+            // 
+            this.subTotalOutput.AutoSize = true;
+            this.subTotalOutput.Location = new System.Drawing.Point(178, 172);
+            this.subTotalOutput.Name = "subTotalOutput";
+            this.subTotalOutput.Size = new System.Drawing.Size(14, 16);
+            this.subTotalOutput.TabIndex = 11;
+            this.subTotalOutput.Text = "0";
+            // 
+            // allTaxOutput
+            // 
+            this.allTaxOutput.AutoSize = true;
+            this.allTaxOutput.Location = new System.Drawing.Point(178, 216);
+            this.allTaxOutput.Name = "allTaxOutput";
+            this.allTaxOutput.Size = new System.Drawing.Size(14, 16);
+            this.allTaxOutput.TabIndex = 12;
+            this.allTaxOutput.Text = "0";
+            // 
+            // afterTaxOutput
+            // 
+            this.afterTaxOutput.AutoSize = true;
+            this.afterTaxOutput.Location = new System.Drawing.Point(178, 263);
+            this.afterTaxOutput.Name = "afterTaxOutput";
+            this.afterTaxOutput.Size = new System.Drawing.Size(14, 16);
+            this.afterTaxOutput.TabIndex = 13;
+            this.afterTaxOutput.Text = "0";
+            // 
+            // startNewOrder
+            // 
+            this.startNewOrder.Location = new System.Drawing.Point(457, 373);
+            this.startNewOrder.Name = "startNewOrder";
+            this.startNewOrder.Size = new System.Drawing.Size(101, 23);
+            this.startNewOrder.TabIndex = 15;
+            this.startNewOrder.Text = "New Order";
+            this.startNewOrder.UseVisualStyleBackColor = true;
+            this.startNewOrder.Click += new System.EventHandler(this.startNewOrder_Click);
+            // 
+            // calculatingTheChange
+            // 
+            this.calculatingTheChange.Location = new System.Drawing.Point(44, 333);
+            this.calculatingTheChange.Name = "calculatingTheChange";
+            this.calculatingTheChange.Size = new System.Drawing.Size(132, 23);
+            this.calculatingTheChange.TabIndex = 16;
+            this.calculatingTheChange.Text = "Calculate Change";
+            this.calculatingTheChange.UseVisualStyleBackColor = true;
+            this.calculatingTheChange.Click += new System.EventHandler(this.calculatingTheChange_Click);
+            // 
+            // sportBaseballs
+            // 
+            this.sportBaseballs.AutoSize = true;
+            this.sportBaseballs.Location = new System.Drawing.Point(38, 23);
+            this.sportBaseballs.Name = "sportBaseballs";
+            this.sportBaseballs.Size = new System.Drawing.Size(68, 16);
+            this.sportBaseballs.TabIndex = 17;
+            this.sportBaseballs.Text = "Baseballs";
+            // 
+            // sportFootball
+            // 
+            this.sportFootball.AutoSize = true;
+            this.sportFootball.Location = new System.Drawing.Point(38, 58);
+            this.sportFootball.Name = "sportFootball";
+            this.sportFootball.Size = new System.Drawing.Size(56, 16);
+            this.sportFootball.TabIndex = 18;
+            this.sportFootball.Text = "Football";
+            // 
+            // numberOfBaseballsInput
+            // 
+            this.numberOfBaseballsInput.Location = new System.Drawing.Point(143, 17);
+            this.numberOfBaseballsInput.Name = "numberOfBaseballsInput";
+            this.numberOfBaseballsInput.Size = new System.Drawing.Size(100, 22);
+            this.numberOfBaseballsInput.TabIndex = 19;
+            // 
+            // enterMoneyInput
+            // 
+            this.enterMoneyInput.Location = new System.Drawing.Point(162, 296);
+            this.enterMoneyInput.Name = "enterMoneyInput";
+            this.enterMoneyInput.Size = new System.Drawing.Size(100, 22);
+            this.enterMoneyInput.TabIndex = 20;
+            // 
+            // numberOfGolfBallsInput
+            // 
+            this.numberOfGolfBallsInput.Location = new System.Drawing.Point(143, 87);
+            this.numberOfGolfBallsInput.Name = "numberOfGolfBallsInput";
+            this.numberOfGolfBallsInput.Size = new System.Drawing.Size(100, 22);
+            this.numberOfGolfBallsInput.TabIndex = 21;
+            // 
+            // numberOfFootballsInput
+            // 
+            this.numberOfFootballsInput.Location = new System.Drawing.Point(143, 52);
+            this.numberOfFootballsInput.Name = "numberOfFootballsInput";
+            this.numberOfFootballsInput.Size = new System.Drawing.Size(100, 22);
+            this.numberOfFootballsInput.TabIndex = 22;
+            // 
+            // moneyGiven
+            // 
+            this.moneyGiven.AutoSize = true;
+            this.moneyGiven.Location = new System.Drawing.Point(60, 296);
+            this.moneyGiven.Name = "moneyGiven";
+            this.moneyGiven.Size = new System.Drawing.Size(67, 16);
+            this.moneyGiven.TabIndex = 23;
+            this.moneyGiven.Text = "Tendered";
+            // 
+            // totalChange
+            // 
+            this.totalChange.AutoSize = true;
+            this.totalChange.Location = new System.Drawing.Point(140, 376);
+            this.totalChange.Name = "totalChange";
+            this.totalChange.Size = new System.Drawing.Size(14, 16);
+            this.totalChange.TabIndex = 25;
+            this.totalChange.Text = "0";
+            // 
+            // changeAmountLabel
+            // 
+            this.changeAmountLabel.AutoSize = true;
+            this.changeAmountLabel.Location = new System.Drawing.Point(50, 373);
+            this.changeAmountLabel.Name = "changeAmountLabel";
+            this.changeAmountLabel.Size = new System.Drawing.Size(54, 16);
+            this.changeAmountLabel.TabIndex = 26;
+            this.changeAmountLabel.Text = "Change";
+            // 
+            // receiptLabel
+            // 
+            this.receiptLabel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.receiptLabel.Location = new System.Drawing.Point(371, 58);
+            this.receiptLabel.Name = "receiptLabel";
+            this.receiptLabel.Size = new System.Drawing.Size(249, 298);
+            this.receiptLabel.TabIndex = 28;
+            // 
+            // receiptButton
+            // 
+            this.receiptButton.Location = new System.Drawing.Point(53, 393);
+            this.receiptButton.Name = "receiptButton";
+            this.receiptButton.Size = new System.Drawing.Size(101, 23);
+            this.receiptButton.TabIndex = 29;
+            this.receiptButton.Text = "Print Receipt";
+            this.receiptButton.UseVisualStyleBackColor = true;
+            this.receiptButton.Click += new System.EventHandler(this.receiptButton_Click);
+            // 
+            // storeTitle
+            // 
+            this.storeTitle.AutoSize = true;
+            this.storeTitle.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.storeTitle.Location = new System.Drawing.Point(340, 21);
+            this.storeTitle.Name = "storeTitle";
+            this.storeTitle.Size = new System.Drawing.Size(65, 16);
+            this.storeTitle.TabIndex = 30;
+            this.storeTitle.Text = "Ball Store";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.Turquoise;
+            this.ClientSize = new System.Drawing.Size(699, 428);
+            this.Controls.Add(this.storeTitle);
+            this.Controls.Add(this.receiptButton);
+            this.Controls.Add(this.receiptLabel);
+            this.Controls.Add(this.changeAmountLabel);
+            this.Controls.Add(this.totalChange);
+            this.Controls.Add(this.moneyGiven);
+            this.Controls.Add(this.numberOfFootballsInput);
+            this.Controls.Add(this.numberOfGolfBallsInput);
+            this.Controls.Add(this.enterMoneyInput);
+            this.Controls.Add(this.numberOfBaseballsInput);
+            this.Controls.Add(this.sportFootball);
+            this.Controls.Add(this.sportBaseballs);
+            this.Controls.Add(this.calculatingTheChange);
+            this.Controls.Add(this.startNewOrder);
+            this.Controls.Add(this.afterTaxOutput);
+            this.Controls.Add(this.allTaxOutput);
+            this.Controls.Add(this.subTotalOutput);
+            this.Controls.Add(this.afterTax);
+            this.Controls.Add(this.allTax);
+            this.Controls.Add(this.amountBeforeTax);
+            this.Controls.Add(this.caluclatingNumbers);
+            this.Controls.Add(this.sportGolfBalls);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label sportGolfBalls;
+        private System.Windows.Forms.Button caluclatingNumbers;
+        private System.Windows.Forms.Label amountBeforeTax;
+        private System.Windows.Forms.Label allTax;
+        private System.Windows.Forms.Label afterTax;
+        private System.Windows.Forms.Label subTotalOutput;
+        private System.Windows.Forms.Label allTaxOutput;
+        private System.Windows.Forms.Label afterTaxOutput;
+        private System.Windows.Forms.Button startNewOrder;
+        private System.Windows.Forms.Button calculatingTheChange;
+        private System.Windows.Forms.Label sportBaseballs;
+        private System.Windows.Forms.Label sportFootball;
+        private System.Windows.Forms.TextBox numberOfBaseballsInput;
+        private System.Windows.Forms.TextBox enterMoneyInput;
+        private System.Windows.Forms.TextBox numberOfGolfBallsInput;
+        private System.Windows.Forms.TextBox numberOfFootballsInput;
+        private System.Windows.Forms.Label moneyGiven;
+     //   private System.Windows.Forms.Label numberOfChange;
+        private System.Windows.Forms.Label totalChange;
+        private System.Windows.Forms.Label changeAmountLabel;
+        private System.Windows.Forms.Label receiptLabel;
+        private System.Windows.Forms.Button receiptButton;
+        private System.Windows.Forms.Label storeTitle;
+    }
+}
+
---
+++ pizza parler/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Media;
+
+namespace pizza_parler
+
+{
+    public partial class Form1 : Form
+    {
+      
+        double taxRate;
+        double priceBaseBalls = 6;
+        double priceFootBalls = 20;
+        double priceGolfBalls = 7;
+
+        //get inputs
+        double numberOfBaseballs;
+        double numberOfFootballs;
+        double numberOfGolfBalls;
+
+
+        //calcuations
+        double subTotal;
+        double taxAmount;
+        double netPay;
+
+        double enterMoney;
+
+       
+        double change;
+
+       
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+     
+
+
+
+        private void caluclatingNumbers_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                taxRate = 0.2;
+
+                numberOfBaseballs = Convert.ToDouble(numberOfBaseballsInput.Text);
+                numberOfFootballs = Convert.ToDouble(numberOfFootballsInput.Text);
+                numberOfGolfBalls = Convert.ToDouble(numberOfGolfBallsInput.Text);
+
+
+                //calcuations
+                subTotal = priceBaseBalls * numberOfBaseballs + priceFootBalls * numberOfFootballs + priceGolfBalls * numberOfGolfBalls;
+                taxAmount = subTotal * taxRate;
+                netPay = subTotal- taxAmount;
+
+                //output
+                subTotalOutput.Text = $"{subTotal.ToString("C")}";
+                allTaxOutput.Text = $"{taxAmount.ToString("C")}";
+                afterTaxOutput.Text = $"{netPay.ToString("C")}";
+            }
+            catch
+            {
+                subTotalOutput.Text = "";
+                allTax.Text = "";
+                afterTax.Text = "ERROR!!!!";
+            }
+        }
+
+        private void calculatingTheChange_Click(object sender, EventArgs e)
+        {
+            enterMoney = Convert.ToDouble(enterMoneyInput.Text);
+            
+
+            change = enterMoney - netPay;
+
+            totalChange.Text = $"{change}";
+
+            if (change < 0)
+            {
+                totalChange.Text = "ERROR";
+                BackColor = Color.Red;
+                SoundPlayer player = new SoundPlayer(Properties.Resources.wrongSound);
+                player.Play();
+            }
+            else 
+            {
+             BackColor = Color.Green;
+               
+            }
+
+        }
+
+  
+
+        private void receiptButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer player = new SoundPlayer(Properties.Resources.printingSound);
+            player.Play();
+            receiptLabel.Text = "                                    Ball Store\n\n";
+            receiptLabel.Refresh();
+            Thread.Sleep(1000);
+            receiptLabel.Text += $"BaseBall cost: {priceBaseBalls * numberOfBaseballs} \n";
+            receiptLabel.Refresh();
+            Thread.Sleep(1000);
+            receiptLabel.Text += $"Football cost:  {priceFootBalls * numberOfFootballs} \n";
+            receiptLabel.Refresh();
+            Thread.Sleep(1000);
+            receiptLabel.Text += $"Golf Ball cost:  {priceGolfBalls * numberOfGolfBalls}\n";
+            receiptLabel.Refresh();
+            Thread.Sleep(1000);
+            receiptLabel.Text += $"Total Price: {subTotal - taxAmount} \n";
+            receiptLabel.Refresh();
+            Thread.Sleep(1000);
+            receiptLabel.Text += $"Change: {change} \n";
+        }
+
+        private void startNewOrder_Click(object sender, EventArgs e)
+        {
+            numberOfBaseballsInput.Text = "";
+            numberOfFootballsInput.Text = "";
+            numberOfGolfBallsInput.Text = "";
+            subTotalOutput.Text = "";
+            allTaxOutput.Text = "";
+            afterTaxOutput.Text = "";
+            enterMoneyInput.Text = "";
+            totalChange.Text = "";
+            receiptLabel.Text = "";
+            BackColor = Color.White;
+        }
+    }
+}
